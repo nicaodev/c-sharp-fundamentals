f761acd7afc2f1e7dbfbf4a9100f92ad177187d1
Nicaodev <alexandre.nicos@gmail.com>
2020-08-16 17:11:01 -0300

    Sobreposicao de Classes

---
+++ Sobreposicao/Sobreposicao/Sobreposicao/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sobreposicao.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+                
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw (double amount) //Com virtual pode ser sobreescrito/sobreposto nas classes que herdam esta superclasse.
+        {
+            Balance -= amount + 5.0; //Com taxa de saque
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ Sobreposicao/Sobreposicao/Sobreposicao/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Sobreposicao.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount) //Sobreescrevendo a função herdada
+        {
+            Balance -= amount; // Sem taxa de saque.
+        }
+    }
+}
